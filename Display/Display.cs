fcc9a901fc4b8ab74711aeb07bc99c09e255aa48
NullAcess <153278494+NullAcess@users.noreply.github.com>
2026-08-01 00:06:14 +0300

    Add memory stats and change visual part

--- Display/Display.cs
+++ Display/Display.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics;
-using ProcessManager.Display.Engine;
+﻿using ProcessManager.Display.Engine;
+using System.Diagnostics;
 
 namespace ProcessManager.Display;
 
@@ -33,7 +33,7 @@ internal class Display
             {
                 case ConsoleKey.D1: ListHandler.Invoke(); break;
                 case ConsoleKey.D2: ExitHandler.Invoke(); break;
-                default: Console.WriteLine("Menu wrong input"); break;
+                default: WrongInputError(); continue;
             }
         }
     }
@@ -42,8 +42,11 @@ internal class Display
     {
         var process = DisplayEngine.ProcessesListLoad();
         const int COUNT_PROCESSES_IN_PAGE = 20;
-        int countOfPages = process.Length / COUNT_PROCESSES_IN_PAGE + 1;
+        int countOfPages = process.Length / COUNT_PROCESSES_IN_PAGE;
         int currentPage = 0;
+
+        if (process.Length % 10 != 0)
+            currentPage++;
 
         while (true)
         {
@@ -58,8 +61,16 @@ internal class Display
 
             for (int i = 0; i < page.Length; i++)
             {
-                Console.WriteLine($"№: {i} \t {page[i].Id}\t| {page[i].ProcessName}");
+                string processNameModifier = page[i].ProcessName;
+                if (page[i].ProcessName.Length >= 25) processNameModifier = page[i].ProcessName[..25] + "...";
+
+                Console.Write($"| Page ID: {i,-2} \t", Console.ForegroundColor = ConsoleColor.DarkGray);
+                Console.Write($"| Name: {processNameModifier,-25} \t", Console.ForegroundColor = ConsoleColor.Yellow);
+                Console.Write($"| Id: {page[i].Id,-5} \t", Console.ForegroundColor = ConsoleColor.DarkGray);
+                Console.Write($"| VirtualMemory64: {page[i].PagedMemorySize64}\n", Console.ForegroundColor = ConsoleColor.Green);
+                Console.ResetColor();
             }
+
             Thread.Sleep(200);
             if (!PageModifier()) return;
 
@@ -68,24 +79,34 @@ internal class Display
                 ConsoleKeyInfo consoleKey = DisplayEngine.GetUserInput();
                 switch (consoleKey.Key)
                 {
-                    case ConsoleKey.E: 
+                    case ConsoleKey.E:
                         {
                             if (currentPage + 1 > countOfPages - 1) currentPage = countOfPages - 1;
                             else currentPage++;
                         }
-                        ;return true;
+                        ; return true;
 
                     case ConsoleKey.Q:
                         {
                             if (currentPage - 1 < 0) currentPage = 0;
                             else currentPage--;
                         }
-                        ;return true;
+                        ; return true;
 
+                    case ConsoleKey.Backspace:
                     case ConsoleKey.Escape: return false;
                 }
                 return false;
             }
         }
+    }
+
+    private void WrongInputError()
+    {
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error #1: Wrong user input");
+        Thread.Sleep(1000);
+        Console.ResetColor();
     }
 }
